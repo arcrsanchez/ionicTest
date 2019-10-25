366532b7c632a1c7149a04bb1d584f73d352d583
Raul Sanchez <arc.rsanchez8@gmail.com>
2019-10-25 16:11:49 +0200

    ionic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'library', pathMatch: 'full' },
-  { path: 'library', loadChildren: './library/library.module#LibraryPageModule' },
+  {
+    path: 'library', children: [
+      { path: '', loadChildren: './library/library.module#LibraryPageModule' },
+      { path: ':bookId', loadChildren: './library/book-detail/book-detail.module#BookDetailPageModule'}
+    ]
+  }
 ];
 
 @NgModule({
